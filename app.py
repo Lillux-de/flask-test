617859daf978baacd7ac74f04e12129720c8b123
a <b>
2020-03-24 00:29:30 +0100

    basic dashboard working now. now add functionality

--- app.py
+++ app.py
@@ -1,18 +1,68 @@
-from flask import Flask, render_template, request, make_response
+from os import getenv #,urandom
+from flask import Flask, render_template, request, make_response, session, escape, redirect, url_for, flash, get_flashed_messages
+from flask_bootstrap import Bootstrap
+import yeelight
+
 
 app = Flask(__name__)
+app.secret_key = getenv('SESSION_KEY') #os.urandom(16)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return("Hello!!")
+    if 'username' in session:
+        return("Hello " + escape(session['username']))
+    else:
+        return redirect(url_for('login') )
 
-@app.route('/hello/')
-@app.route('/hello/<string:name>')
-def hello(name=None):
-    resp = make_response(render_template('greet.html', name=name))
-    resp.set_cookie('name', name)
-    return resp
+
+
+@app.route('/dashboard')
+def dashboard():
+
+    yeelight = {
+        "id" : 1,
+        "name" : "yeelight bulb",
+        "ip" : "192.168.178.40"
+    }
+    yeestrip = {
+        "id" : 2,
+        "name" : "yeelight strip",
+        "ip" : "192.168.178.40"
+    }
+    lamps = [yeelight, yeestrip]
+    
+    return render_template('dashboard.html', lamps=lamps)
+
+@app.route('/lamp/')
+@app.route('/lamp/<int:lampid>')
+def lamp(lampid):
+    if lampid == None:
+        return redirect(url_for('dashboard'))
+
+    return render_template('lamp.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        flash('successfull login')
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    #remove username from session if its there
+    session.pop('username', None)
+    flash('successfull logout')
+    return redirect(url_for('index'))
+
 
 @app.route('/about')
 def about():
-    return 'This is a flask (python) test app.'
+    return render_template('about.html')

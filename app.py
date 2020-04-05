976144b5429eb7e5982610b0eab1b8655d762017
a <b>
2020-04-05 10:08:21 +0200

    yeelights work
    yeeligths work now without solid json template. json gets analyzed step by step (server side)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from os import getenv #,urandom
-from flask import Flask, render_template, request, make_response, session, escape, redirect, url_for, flash, get_flashed_messages, app, logging
+from flask import Flask, render_template, request, session, escape, redirect, url_for, flash, get_flashed_messages, app, jsonify
 import yeelight as yee
+import json
 
 app = Flask(__name__)
 app.secret_key = getenv('SESSION_KEY') #os.urandom(16) FIXME:
@@ -16,18 +17,18 @@ yeelight = {
 yeestrip = {
 	"id" : 1,
 	"name" : "yeelight strip",
-	"ip" : "192.168.178.40",
-	"bulb" : yee.Bulb("192.168.178.40"),
+	"ip" : "192.168.178.35",
+	"bulb" : yee.Bulb("192.168.178.35"),
 	"type" : "yeelight"
 }
 blinkstick = {
 	"id" : 2,
 	"name": "blinkstick",
-	"ip" : "192.168.178.40",
+	"ip" : "localhost",
 	"bulb": None,
 	"type" : "blinkstick"
 }
-lamps = [yeelight, yeestrip, blinkstick] #TODO: add blinkstick
+lamps = [yeelight, yeestrip, blinkstick]
 
 
 @app.route('/')
@@ -49,12 +50,28 @@ def lamp(lampid=None):
 		return redirect(url_for('dashboard'))
 	else:
 		if request.method == 'POST':
-			data = request.form
-			lamps[lampid]['bulb'].set_rgb( int(data['r']) , int(data['g']) , int(data['b']) ) #TODO: switch case lamps[lampid].type for blinkstick and yeelight
-			#print("r" + data['r'] + " g" + data['g'] + " b" + data['b'])
+			data = request.get_json()
+			#print(data)
+			lm = lamps[lampid]['bulb']
+
+			#TODO: implement json logic analysis here, then call functions below.
+
+			if 'color' in data:
+				lm.set_rgb(  int(data['color']['r']) , int(data['color']['g']) , int(data['color']['b']) )
+			if 'brightness' in data:
+				lm.set_brightness( int(data['brightness']) )
 			return "got the data. thanks server."
 		else:
 			return render_template('lamp.html', lamp = lampid)
+
+def create_flow():
+	pass
+
+def change_color():
+	pass
+
+def change_brightness():
+	pass
 
 
 @app.route('/logout')
